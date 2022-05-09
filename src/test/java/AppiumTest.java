5459b9f60618c7e1bf7c205370997da0c7657b26
jguitron <jguitron@ashleyfurniture.com>
2022-05-09 16:30:47 -0400

    Initial commit

---
+++ src/test/java/AppiumTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+    @Test
+    public void NativeAppAndriod ()
+    {
+        System.out.println("NativeAppAndriod");
+    }
+
+    @Test
+    public void IOSApps ()
+    {
+        System.out.println("IOSApps");
+    }
+}
